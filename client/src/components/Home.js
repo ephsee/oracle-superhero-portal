50d17bfe3a6d1bce0e5660de8d187ca6092de5f5
maxjmar <86731281+maxjmar@users.noreply.github.com>
2022-03-25 14:42:34 -0400

    added divs to home

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,8 +1,13 @@
 
 function Home() {
     return (
-      <div className="Initial">
+      <div> 
+        <h2>Welcome, insert user</h2>
+        <div><button> Allies </button></div>
+        <div><button> Gadgets </button></div>
+        <div><button> Villains </button></div>
       </div>
+      
     );
   }
   
