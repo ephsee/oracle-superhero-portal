a556d5f719774cfc3af7b3f97e3310682d4b9504
maxjmar <86731281+maxjmar@users.noreply.github.com>
2022-03-25 08:28:56 -0400

    seeds started

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,3 +24,34 @@ end
 10.times do
     Ally.create(name: Faker::Name.name, affiliation:"good or bad", image:"pic goes here", hero_id: Hero.all.sample.id)
 end
+puts 'seeding locations...'
+# Locations
+
+puts 'seeding heroes...'
+# Heroes
+Hero.create(name: "Batman", alter_ego: "Bruce Wayne", base: Faker::Nation.capital_city, image: "pic goes here")
+Hero.create(name: "Superman", alter_ego: "Clark Kent", base: Faker::Nation.capital_city, image: "pic goes here")
+Hero.create(name: "Wonder Woman", alter_ego: "Diana of Themyscira", base: Faker::Nation.capital_city, image: "pic goes here")
+
+puts 'seeding villains'
+# Villains
+Villain.create(name: 'Joker', alter_ego: 'Unknown', at_large: true, most_wanted: rand(1..10), alignment:"bad or real bad", image:"pic goes here", hero_id: Hero.all.sample.id )
+Villain.create(name: 'Catwoman', alter_ego: 'Selina Kyle', at_large: true, most_wanted: rand(1..10), alignment:"bad or real bad", image:"pic goes here", hero_id: Hero.all.sample.id )
+Villain.create(name: 'Killer Moth', alter_ego: 'Drury Walker', at_large: false, most_wanted: rand(1..10), alignment:"bad or real bad", image:"pic goes here", hero_id: Hero.all.sample.id )
+Villain.create(name: 'Carmine Falcone', alter_ego: 'N/A', at_large: false, most_wanted: rand(1..10), alignment:"bad or real bad", image:"pic goes here", hero_id: Hero.all.sample.id )
+Villain.create(name: 'Penguin', alter_ego: 'Oswald Cobblepot', at_large: true, most_wanted: rand(1..10), alignment:"bad or real bad", image:"pic goes here", hero_id: Hero.all.sample.id )
+Villain.create(name: 'Red Hood', alter_ego: 'Jason Todd', at_large: true, most_wanted: rand(1..10), alignment:"bad or real bad", image:"pic goes here", hero_id: Hero.all.sample.id )
+Villain.create(name: 'Salvatore Maroni', alter_ego: 'N/A', at_large: false, most_wanted: rand(1..10), alignment:"bad or real bad", image:"pic goes here", hero_id: Hero.all.sample.id )
+Villain.create(name: 'Darkseid', alter_ego: 'N/A', at_large: true, most_wanted: rand(1..10), alignment:"bad or real bad", image:"pic goes here", hero_id: Hero.all.sample.id )
+# Superman
+Villain.create(name: 'Lex Luthor', alter_ego: 'N/A', at_large: true, most_wanted: rand(1..10), alignment:"bad or real bad", image:"pic goes here", hero_id: Hero.all.sample.id )
+Villain.create(name: 'Toyman', alter_ego: 'Winslow Scott', at_large: false, most_wanted: rand(1..10), alignment:"bad or real bad", image:"pic goes here", hero_id: Hero.all.sample.id )
+Villain.create(name: 'Metallo', alter_ego: 'John Corben', at_large: false, most_wanted: rand(1..10), alignment:"bad or real bad", image:"pic goes here", hero_id: Hero.all.sample.id )
+Villain.create(name: "Whisper A'Daire", alter_ego: 'N/A', at_large: true, most_wanted: rand(1..10), alignment:"bad or real bad", image:"pic goes here", hero_id: Hero.all.sample.id )
+Villain.create(name: 'Darkseid', alter_ego: 'N/A', at_large: true, most_wanted: rand(1..10), alignment:"bad or real bad", image:"pic goes here", hero_id: Hero.all.sample.id )
+# Wonder Woman
+Villain.create(name: 'Cheetah', alter_ego: 'Barbara Ann Minerva', at_large: true, most_wanted: rand(1..10), alignment:"bad or real bad", image:"pic goes here", hero_id: Hero.all.sample.id )
+Villain.create(name: 'Maxwell Lord', alter_ego: 'N/A', at_large: true, most_wanted: rand(1..10), alignment:"bad or real bad", image:"pic goes here", hero_id: Hero.all.sample.id )
+Villain.create(name: 'Giganta', alter_ego: 'Doris Zuell', at_large: false, most_wanted: rand(1..10), alignment:"bad or real bad", image:"pic goes here", hero_id: Hero.all.sample.id )
+Villain.create(name: 'Queen of Fables', alter_ego: 'Tsarita', at_large: true, most_wanted: rand(1..10), alignment:"bad or real bad", image:"pic goes here", hero_id: Hero.all.sample.id )
+Villain.create(name: 'Darkseid', alter_ego: 'N/A', at_large: true, most_wanted: rand(1..10), alignment:"bad or real bad", image:"pic goes here", hero_id: Hero.all.sample.id )
