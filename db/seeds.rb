6dadc92b4638567ca2deaaba3ce0d4c123ffa775
maxjmar <86731281+maxjmar@users.noreply.github.com>
2022-03-25 08:49:53 -0400

    more seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,28 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-10.times do
-    Hero.create(name: Faker::Name.name, alter_ego: Faker::Name.name, base: Faker::Nation.capital_city, image: "pic goes here")
-end
+# 10.times do
+#     Hero.create(name: Faker::Name.name, alter_ego: Faker::Name.name, base: Faker::Nation.capital_city, image: "pic goes here")
+# end
 
-10.times do
-    Villain.create(name: Faker::Name.name, alter_ego: Faker::Name.name, at_large: false, most_wanted: rand(1..10), alignment:"bad or real bad", image:"pic goes here", hero_id: Hero.all.sample.id )
-end
+# 10.times do
+#     Villain.create(name: Faker::Name.name, alter_ego: Faker::Name.name, at_large: false, most_wanted: rand(1..10), alignment:"bad or real bad", image:"pic goes here", hero_id: Hero.all.sample.id )
+# end
 
-10.times do
-    Location.create(name: Faker::Nation.capital_city, villain_id: Villain.all.sample.id)
-end
+# 10.times do
+#     Location.create(name: Faker::Nation.capital_city, villain_id: Villain.all.sample.id)
+# end
 
-10.times do
-    Gadget.create(item_name:"", quantity:rand(1..10), hero_id: Hero.all.sample.id, image:"pic goes here")
-end
+# 10.times do
+#     Gadget.create(item_name:"", quantity:rand(1..10), hero_id: Hero.all.sample.id, image:"pic goes here")
+# end
 
-10.times do
-    Ally.create(name: Faker::Name.name, affiliation:"good or bad", image:"pic goes here", hero_id: Hero.all.sample.id)
-end
+# 10.times do
+#     Ally.create(name: Faker::Name.name, affiliation:"good or bad", image:"pic goes here", hero_id: Hero.all.sample.id)
+# end
 puts 'seeding locations...'
 # Locations
-
+Location.create(name: 'Gotham City', villain_id: Villain.all.sample.id)
+Location.create(name: 'Metropolis', villain_id: Villain.all.sample.id)
+Location.create(name: 'Washington D.C.', villain_id: Villain.all.sample.id)
+Location.create(name: 'Apokolypse', villain_id: Villain.all.sample.id)
 puts 'seeding heroes...'
 # Heroes
 Hero.create(name: "Batman", alter_ego: "Bruce Wayne", base: Faker::Nation.capital_city, image: "pic goes here")
