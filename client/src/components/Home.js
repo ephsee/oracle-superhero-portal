0540ddf5e7eff4bac654c3bde60e72077b8d45ff
Francois Spies <fcspies@gmail.com>
2022-03-31 09:38:44 -0400

    better login

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -14,15 +14,6 @@ function Home({hero, logout}) {
       alert("LOG OUT COMPLETE - DATA SECURED")
     }
 
-    // const where = hero.locations[0].name
-
-    // console.log(where)
-
-    // USE LOCATIONS TO MAP VILLAINS AT LARGE FOR TODOLIST
-
-    // ask to make sure you want to log out
-
-    // redirect to start page
     if (hero.error === 'No Active Heros') {
       return ("nothing to see here")
     } else {
@@ -71,49 +62,6 @@ function Home({hero, logout}) {
       );
     }
 
-    // return (
-    //   <>
-
-    //     <div>
-    //       <NavLink
-    //         to="/authorized_hero">
-    //         Details
-    //       </NavLink>
-
-    //       <NavLink
-    //         to="/allies">
-    //         Allies
-    //       </NavLink>
-
-    //       <NavLink
-    //         to="/gadgets">
-    //         Gadgets
-    //       </NavLink>
-
-    //       <NavLink
-    //         to="/villains">
-    //         Villains
-    //       </NavLink>
-
-    //       <NavLink
-    //         to="/"
-    //         onClick={(e) => signout(e)}
-    //         >
-    //         Logout
-    //       </NavLink>
-    //     </div>
-
-    //   <div> 
-    //     <hr></hr>
-    //     <h2>{hero.alter_ego} from {hero.base}</h2>
-    //     <hr></hr>
-    //     <h3>Happy Hunting</h3>
-    //     <img src={hero.image} alt={hero.name} width="300px"/>
-    //     {/* <p>{hero.name} has been vigilantly protecting the innocent and defending {where}</p> */}
-    //   </div>
-
-    //   </>
-    // );
   }
   
   export default Home;
