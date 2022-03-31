ac51287f46f31c6f3ba11e23e5942ff4133c79ed
Francois Spies <fcspies@gmail.com>
2022-03-31 09:30:24 -0400

    better login

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,16 +3,16 @@ import {NavLink} from 'react-router-dom'
 
 function Home({hero, logout}) {
 
+  console.log(hero)
+
   function signout(e) {
 
     fetch("/logout", {
       method: 'DELETE'})
       .then(r=>r.json())
       .then(logout)
-      alert("log out complete - data secured")
+      alert("LOG OUT COMPLETE - DATA SECURED")
     }
-
-    console.log(hero)
 
     // const where = hero.locations[0].name
 
@@ -23,50 +23,97 @@ function Home({hero, logout}) {
     // ask to make sure you want to log out
 
     // redirect to start page
-
-    return (
-      <>
-
-        <div>
-          <NavLink
-            to="/authorized_hero">
-            Details
-          </NavLink>
-
-          <NavLink
-            to="/allies">
-            Allies
-          </NavLink>
-
-          <NavLink
-            to="/gadgets">
-            Gadgets
-          </NavLink>
-
-          <NavLink
-            to="/villains">
-            Villains
-          </NavLink>
-
-          <NavLink
-            to="/"
-            onClick={(e) => signout(e)}
-            >
-            Logout
-          </NavLink>
+    if (hero.error === 'No Active Heros') {
+      return ("nothing to see here")
+    } else {
+      return (
+        <>
+  
+          <div>
+            <NavLink
+              to="/authorized_hero">
+              Details
+            </NavLink>
+  
+            <NavLink
+              to="/allies">
+              Allies
+            </NavLink>
+  
+            <NavLink
+              to="/gadgets">
+              Gadgets
+            </NavLink>
+  
+            <NavLink
+              to="/villains">
+              Villains
+            </NavLink>
+  
+            <NavLink
+              to="/"
+              onClick={(e) => signout(e)}
+              >
+              Logout
+            </NavLink>
+          </div>
+  
+        <div> 
+          <hr></hr>
+          <h2>{hero.alter_ego} from {hero.base}</h2>
+          <hr></hr>
+          <h3>Happy Hunting</h3>
+          <img src={hero.image} alt={hero.name} width="300px"/>
+          {/* <p>{hero.name} has been vigilantly protecting the innocent and defending {where}</p> */}
         </div>
+  
+        </>
+      );
+    }
 
-      <div> 
-        <hr></hr>
-        <h2>{hero.alter_ego} from {hero.base}</h2>
-        <hr></hr>
-        <h3>Happy Hunting</h3>
-        <img src={hero.image} alt={hero.name} width="300px"/>
-        {/* <p>{hero.name} has been vigilantly protecting the innocent and defending {where}</p> */}
-      </div>
+    // return (
+    //   <>
 
-      </>
-    );
+    //     <div>
+    //       <NavLink
+    //         to="/authorized_hero">
+    //         Details
+    //       </NavLink>
+
+    //       <NavLink
+    //         to="/allies">
+    //         Allies
+    //       </NavLink>
+
+    //       <NavLink
+    //         to="/gadgets">
+    //         Gadgets
+    //       </NavLink>
+
+    //       <NavLink
+    //         to="/villains">
+    //         Villains
+    //       </NavLink>
+
+    //       <NavLink
+    //         to="/"
+    //         onClick={(e) => signout(e)}
+    //         >
+    //         Logout
+    //       </NavLink>
+    //     </div>
+
+    //   <div> 
+    //     <hr></hr>
+    //     <h2>{hero.alter_ego} from {hero.base}</h2>
+    //     <hr></hr>
+    //     <h3>Happy Hunting</h3>
+    //     <img src={hero.image} alt={hero.name} width="300px"/>
+    //     {/* <p>{hero.name} has been vigilantly protecting the innocent and defending {where}</p> */}
+    //   </div>
+
+    //   </>
+    // );
   }
   
   export default Home;
